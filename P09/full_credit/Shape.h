72ada32e32a730b0f05f93c045f05b489cf09687
Rabib001 <142938485+Rabib001@users.noreply.github.com>
2023-11-05 17:14:14 -0600

    Create Shape.h

---
+++ P09/full_credit/Shape.h
@@ -0,0 +1,14 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include<string>
+
+class Shape
+{
+public:
+    virtual std::string name();
+    virtual double area();
+    std::string to_string();
+};
+
+#endif //SHAPE_H
